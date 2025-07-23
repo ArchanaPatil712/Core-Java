62e661209445b8e16fb598f7b0255651deeae17d
ARCHANA <ARCHANA@LAPTOP-6D12L83D>
2025-07-23 21:42:30 +0530

    Today,I have learned the conditional statements.

---
+++ java coding/src/conditionals.java
@@ -0,0 +1,17 @@
+
+
+public class conditionals {
+	public static void main(String[] args) {
+		int n1=9;
+		int n2=3;
+		if(n1==n2) {
+			System.out.println(n1 +" is equal to "+ n2);
+		}
+		else if(n1>n2) {
+			System.out.println(n1+" is greater than "+n2);
+		}
+		else {
+			System.out.println(n1+" is smaller than "+n2);
+		}
+	}
+}
